ad0ef9317b025cde2b53bc8ce84e1c72a39d1a99
hoshino <krallice.untitled@gmail.com>
2023-10-18 18:22:06 +0900

    build: update setup.py with package information

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
 from setuptools import setup, find_packages
 
-setup(name="sdscripts", packages=find_packages())
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name="sdscripts",
+    version="0.1.0",
+    install_requires=_requires_from_file("requirements.txt"),
+    packages=find_packages("."),
+    package_dir={"": "."},
+)
