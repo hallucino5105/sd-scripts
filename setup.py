a6f590ef9329abb6fe20db40f3f8b9657d31d8ed
hoshino <krallice.untitled@gmail.com>
2023-10-18 18:40:45 +0900

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="sdscripts",
-    version="0.1.0",
-    packages=find_packages("."),
-    package_dir={"": "."},
-)
+setup(name="sdscripts", version="0.1.0")
