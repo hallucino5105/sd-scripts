332adcb406b8f27acd48de95ea1b3de6ebaeb8dd
hoshino <krallice.untitled@gmail.com>
2023-10-18 18:23:15 +0900

    build: Remove unused code in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from setuptools import setup, find_packages
 
-
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
 setup(
     name="sdscripts",
     version="0.1.0",
-    install_requires=_requires_from_file("requirements.txt"),
     packages=find_packages("."),
     package_dir={"": "."},
 )
